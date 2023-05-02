faeee16d2a5c72381c7d4706a2dfd47f0ba7e79d
lukegriley <lukegough@me.com>
2023-05-02 13:15:26 -0400

    sdfjsdf

---
+++ src/FrameInterface.tsx
@@ -0,0 +1,24 @@
+import { FrameData } from "./frameData"
+import Whiteboard from "./board"
+import Thumbnail from "./Thumbnail"
+import "./styles/FrameInterface.css";
+import { useState } from "react";
+
+export interface FrameInterfaceProps {
+    frames: FrameData[]
+}
+export default function FrameInterface(props: FrameInterfaceProps) {
+
+    const [currentFrame,setCurrentFrame] = useState<FrameData>(props.frames[0]);
+
+    return (
+        <>
+        <div className="Frames">
+            {props.frames.map((object:FrameData, i) => <Thumbnail setCurrentFrame={setCurrentFrame} key={i} data={object}/>)}   
+        </div>
+        <div className="whiteboardDisplay">
+            <Whiteboard currentFrame={currentFrame} setCurrentFrame={setCurrentFrame}/>
+        </div>
+        </>
+    )
+}
--- src/private/api.ts
+++
@@ -1,4 +0,0 @@
-
-
-const ACCESS_TOKEN = "pk.eyJ1IjoibHJpbGV5MiIsImEiOiJjbGZsazAyNmcwMmxzM3lvYzB6Y3ljbWpmIn0.kVDZeMtUGeNMdGJTc8GCnA";
-export {ACCESS_TOKEN};
