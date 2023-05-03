528f151817e33ac150f11b65a18312b334f12474
OwenAnderson <owen_anderson@brown.edu>
2023-05-03 16:44:04 -0400

    colorwheel works

--- src/FrameInterface.tsx
+++ src/FrameInterface.tsx
@@ -1,24 +1,28 @@
-import { FrameData } from "./frameData"
-import Whiteboard from "./board"
-import Thumbnail from "./Thumbnail"
+import { FrameData } from "./frameData";
+import Whiteboard from "./board";
+import Thumbnail from "./Thumbnail";
 import "./styles/FrameInterface.css";
 import { useState } from "react";
 
 export interface FrameInterfaceProps {
-    frames: FrameData[]
+  frames: FrameData[];
 }
 export default function FrameInterface(props: FrameInterfaceProps) {
+  const [currentFrame, setCurrentFrame] = useState<FrameData>(props.frames[0]);
 
-    const [currentFrame,setCurrentFrame] = useState<FrameData>(props.frames[0]);
-
-    return (
-        <>
-        <div className="Frames">
-            {props.frames.map((object:FrameData, i) => <Thumbnail setCurrentFrame={setCurrentFrame} key={i} data={object}/>)}   
-        </div>
-        <div className="whiteboardDisplay">
-            <Whiteboard currentFrame={currentFrame} setCurrentFrame={setCurrentFrame}/>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="Frames">
+        {props.frames.map((object: FrameData, i) => (
+          <Thumbnail setCurrentFrame={setCurrentFrame} key={i} data={object} />
+        ))}
+      </div>
+      <div className="whiteboardDisplay">
+        <Whiteboard
+          currentFrame={currentFrame}
+          setCurrentFrame={setCurrentFrame}
+        />
+      </div>
+    </>
+  );
 }
