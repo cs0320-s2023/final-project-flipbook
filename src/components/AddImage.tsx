b72b32aee7cee1e6c91060c9061081cff3d0db08
Ben Maizes <benjamin_maizes@brown.edu>
2023-05-09 22:33:29 -0400

    re pull this owen bozo:

--- src/components/AddImage.tsx
+++ src/components/AddImage.tsx
@@ -13,47 +13,47 @@ export const AddImage: React.FC<AddImageProps> = ({
   boardRef,
   props,
 }) => {
-  // function drawLine(
-  //   x1: number,
-  //   y1: number,
-  //   x2: number,
-  //   y2: number,
-  //   color: string,
-  //   width: number,
-  //   opacity: number
-  // ): void {
-  //   if (boardRef != null && boardRef.current != null) {
-  //     const context: undefined | CanvasRenderingContext2D | null =
-  //       boardRef.current.getContext("2d");
-  //     if (context instanceof CanvasRenderingContext2D) {
-  //       context.globalAlpha = opacity
-  //       context.beginPath();
-  //       context.moveTo(x1, y1);
-  //       context.lineTo(x2, y2);
-  //       context.strokeStyle = color;
-  //       context.lineCap = "round"; //make it so that a stroke is a circle, not a rectangle
-  //       context.lineWidth = width;
-  //       context.stroke();
-  //       context.closePath();
-  //       context.globalAlpha = 1.0
-  //     }
-  //   }
-  // }
+  function drawLine(
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    color: string,
+    width: number,
+    opacity: number
+  ): void {
+    if (boardRef != null && boardRef.current != null) {
+      const context: undefined | CanvasRenderingContext2D | null =
+        boardRef.current.getContext("2d");
+      if (context instanceof CanvasRenderingContext2D) {
+        context.globalAlpha = opacity
+        context.beginPath();
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.strokeStyle = color;
+        context.lineCap = "round"; //make it so that a stroke is a circle, not a rectangle
+        context.lineWidth = width;
+        context.stroke();
+        context.closePath();
+        context.globalAlpha = 1.0
+      }
+    }
+  }
 
-  // function drawAction(a: Action) {
-  //   for (var i = 0; i < a.pos.length; i++) {
-  //     console.log(a.radius);
-  //     drawLine(
-  //       a.pos[i][0],
-  //       a.pos[i][1],
-  //       a.pos[i][2],
-  //       a.pos[i][3],
-  //       a.color,
-  //       a.radius,
-  //       a.opacity
-  //     );
-  //   }
-  // }
+  function drawAction(a: Action) {
+    for (var i = 0; i < a.pos.length; i++) {
+      console.log(a.radius);
+      drawLine(
+        a.pos[i][0],
+        a.pos[i][1],
+        a.pos[i][2],
+        a.pos[i][3],
+        a.color,
+        a.radius,
+        a.opacity
+      );
+    }
+  }
 
   function addImage() {
     if (boardRef != null && boardRef.current != null) {
